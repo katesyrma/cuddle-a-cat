f09fc2c946576f7b6f5db492d1aa90d4b8fb331e
Alessandro Carta <alessandro.carta91@gmail.com>
2021-05-27 17:31:21 +0200

    implementing booking feature

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def new
+    @cat = Cat.find(params[:cat_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @cat = Cat.find(params[:cat_id])
+    @booking.cat = @cat
+    @booking.user = current_user
+    @booking.save
+    redirect_to bookings(current_user)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to cat_bookings_path(current_user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out)
+  end
+
+end

f3cab79c2960ddc34398a3acd7a3fa72e8a5b2de
Kate Syrmakesi <kate.syrmakesi@gmail.com>
2021-05-27 18:40:40 +0200

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_05_27_101216) do
 
   # These are extensions that must be enabled in order to support this database
